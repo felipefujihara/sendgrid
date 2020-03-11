0f0cc3a5e2cc6c32dbbfa6826950febb4dc8f12e
Felipe <felipe.fujihara@ltmfidelidade.com.br>
2020-03-11 16:04:37 -0300

    creating solution

---
+++ SendGridPOC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SendGridPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("felipe@fujihara.com", "Felipe Fujihara");
+            var subject = "SendGrid Test";
+            var to = new EmailAddress("felipe.fujihara@gmail.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
